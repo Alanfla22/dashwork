7534ef62683ea121b2b8d0c5f2e7b0dd285e7fea
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-04-29 12:33:59 -0300

    Update histPlot.js

--- modules/histPlot.js
+++ modules/histPlot.js
@@ -2,7 +2,7 @@
 import {filtrarTable} from "./filtrarTable.js";
 
 const xSize = 500;
-const ySize = 300;
+const ySize = 345;
 const margin = 40;        
 const xMax = xSize - margin*2;
 const yMax = ySize - margin*2;        

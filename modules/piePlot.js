838ae82b84d0104f6f2e2cae5476aeaf415238ec
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-05-03 10:33:30 -0300

    Update piePlot.js

--- modules/piePlot.js
+++ modules/piePlot.js
@@ -82,6 +82,7 @@ export function pieSvg (dados, option) {
         .append("path")        
         .attr("d", arc)
         .attr("fill", d => color(d.data[0]))
+        .style("transition", "0.3s")
         .on("mouseover", function () {
             d3.select(this).attr("fill", "hsl(196 70 88)");
               

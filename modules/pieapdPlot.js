539c1ec8c80eb7437caaed4b7145c4c31c3fa52b
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-05-04 13:01:44 -0300

    Update pieapdPlot.js

--- modules/pieapdPlot.js
+++ modules/pieapdPlot.js
@@ -47,7 +47,7 @@ export function pieapdSvg (dados, option) {
         .attr("width", 30)
         .attr("height", 30)
         .attr("fill", d => color(d.data[0]))
-        .style("cursor" "pointer")  
+        .style("cursor", "pointer") 
         .style("transition", "0.3s")  
         .on("mouseover", function () {
             d3.select(this).attr("fill", "hsl(196 70 88)");
@@ -78,7 +78,7 @@ export function pieapdSvg (dados, option) {
         .append("path")        
         .attr("d", arc)
         .attr("fill", d => color(d.data[0]))
-        .style("cursor" "pointer")  
+        .style("cursor", "pointer")  
         .style("transition", "0.3s")
         .on("mouseover", function () {
             d3.select(this).attr("fill", "hsl(196 70 88)");

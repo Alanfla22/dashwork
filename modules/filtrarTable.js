0e9696795cbd05f000c7ac73eae27312d4415cdc
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-06-14 20:40:14 -0300

    Delete modules/filtrarTable.js

--- modules/filtrarTable.js
+++
@@ -1,8 +0,0 @@
-import {tabulate} from "./tablePlot.js";
-
-export function filtrarTable (dados, key, selecao) {
-
-    const result = dados.filter((d) => d[key] == selecao);
-
-    tabulate(result);
-};

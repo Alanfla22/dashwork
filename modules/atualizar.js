008533d2526e027ae104d06866bccef31748f78d
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-06-14 20:42:02 -0300

    Update atualizar.js

--- modules/atualizar.js
+++ modules/atualizar.js
@@ -8,7 +8,73 @@ export function atualizarInputs (input) {
         objectFiles[file.name] = URL.createObjectURL(file);
     })
 
-   
-    return objectFiles;
+    const base = d3.dsv(";", objectFiles["base.csv"]).then(data => {
 
-}
+
+            var dados = [];
+
+            data.forEach((item) => {
+                
+                if ((item.Tarefa == "Validar Resolu��o das Ocorr�ncias de Instru��o") || (item.Tarefa == "Registrar Ocorr�ncias de Instru��o")) {
+
+                    item.Data = item[data.columns[9]].split(" ")[0]
+                    item.Tipo = item["Tipo Solicita��o"];
+                    item.Super = item["Nome Superintendencia"];
+                    item.Agencia = item["Nome Ag�ncia"];                    
+                    dados.push(item);
+
+                };               
+                
+            });
+            
+            return dados;
+        
+        });
+
+    const baseapd = d3.dsv(";", objectFiles["baseapd.csv"]).then(data => {
+
+
+            var dados = [];
+
+            data.forEach((item) => {
+                
+                item.Data = item["Data de Cria��o"];
+                item.Nome = item["Nome Cliente"];
+                item.Tarefa = item["Situa��o"];
+                item.Super = item["Superintend�ncia"].split("-")[1];
+                item.Agencia = item["Ag�ncia"].split("-")[1];             
+                item.Tipo = item["Tipo IC"];
+                dados.push(item);
+                
+            });
+            
+            return dados;
+        
+        });
+        
+
+    const baseguarda = d3.dsv(";", objectFiles["guarda.csv"]).then(data => {
+
+
+           
+            return data;
+        
+        });
+
+    const basepaths = d3.dsv(",", objectFiles["BNB_Paths.csv"]).then(data => {
+
+
+            return data;
+        
+        });          
+
+    return {
+        "base": base,
+        "baseapd": baseapd,
+        "baseguarda": baseguarda,
+        "basepaths": basepaths
+    }
+
+}       
+
+

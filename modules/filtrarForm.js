d29e2edb35c46078668eb5db02e0b5f285b2e324
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-06-14 18:54:17 -0300

    Update filtrarForm.js

--- modules/filtrarForm.js
+++ modules/filtrarForm.js
@@ -36,65 +36,10 @@ export function filtrarForm (filtros, inputs) {
     fieldTipo.selectAll("label").remove();
     fieldTipo.selectAll("br").remove();
     
-    const base = d3.dsv(";", inputs["base.csv"]).then(data => {
-
-
-            var dados = [];
-
-            data.forEach((item) => {
-                
-                if ((item.Tarefa == "Validar Resolu��o das Ocorr�ncias de Instru��o") || (item.Tarefa == "Registrar Ocorr�ncias de Instru��o")) {
-
-                    item.Data = item[data.columns[9]].split(" ")[0]
-                    item.Tipo = item["Tipo Solicita��o"];
-                    item.Super = item["Nome Superintendencia"];
-                    item.Agencia = item["Nome Ag�ncia"];                    
-                    dados.push(item);
-
-                };               
-                
-            });
-            
-            return dados;
-        
-        });
-        
-
-    const baseapd = d3.dsv(";", inputs["baseapd.csv"]).then(data => {
-
-
-            var dados = [];
-
-            data.forEach((item) => {
-                
-                item.Data = item["Data de Cria��o"];
-                item.Nome = item["Nome Cliente"];
-                item.Tarefa = item["Situa��o"];
-                item.Super = item["Superintend�ncia"].split("-")[1];
-                item.Agencia = item["Ag�ncia"].split("-")[1];             
-                item.Tipo = item["Tipo IC"];
-                dados.push(item);
-                
-            });
-            
-            return dados;
-        
-        });
-        
-
-    const baseguarda = d3.dsv(";", inputs["guarda.csv"]).then(data => {
-
-            
-            return data;
-        
-        });
-
-    const basepaths = d3.dsv(",", inputs["BNB_Paths.csv"]).then(data => {
-
-
-            return data;
-        
-        });        
+    const base = dadosInput(inputs).base;
+    const baseapd = dadosInput(inputs).baseapd;
+    const baseguarda = dadosInput(inputs).baseguarda;
+    const basepaths = dadosInput(inputs).basepaths;     
     
     baseguarda.then((guarda) => {   
 

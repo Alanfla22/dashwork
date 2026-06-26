42f28dc5ad14bfdf8efecf50ac2ac4041628cb51
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-06-26 18:37:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ d3.selectAll(".super")
 
 const input = document.getElementById("myfile");
 
-input.addEventListener("submit", () => {
+input.addEventListener("change", () => {
 
     const inputs = atualizarInputs(input);
   

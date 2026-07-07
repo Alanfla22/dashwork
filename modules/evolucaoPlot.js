cd936f8d4b6899b23a2d72bdf726a7a73649d4be
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-07 17:53:33 -0300

    Update evolucaoPlot.js

--- modules/evolucaoPlot.js
+++ modules/evolucaoPlot.js
@@ -77,6 +77,6 @@ export function lineSvg () {
     .style("cursor", "pointer")
     .style("transition", "0.3s")
     .append("title")
-    .text((d, i) => `${historico[i + 1].data} - ${d.total + (d.calc / 2)} média`);
+    .text((d, i) => `&#8593 ${historico[i].data}`);
 
 }

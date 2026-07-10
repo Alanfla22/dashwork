5a9c65bb401dfd1c7b43b4ddcc22090c10a1c1da
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-10 15:58:09 -0300

    Create estadoPlot.js

---
+++ modules/estadoPlot.js
@@ -0,0 +1,42 @@
+
+const mapa = d3.select("#mapa");
+
+export function estadoPlot (groupSuper, groupApdSuper) {
+
+    mapa.selectAll(".super")
+    .select("title").remove();
+
+    const noteList = mapa.selectAll(".super")._groups[0];
+
+    const supersObject = {};
+
+    for (var i = 0; i < noteList.length; i++) {
+
+        supersObject[noteList[i].id] = [];
+        
+    }  
+
+    Object.keys(groupSuper).forEach((key) => {
+        supersObject[key] = Object.values(supersObject[key]).concat(groupSuper[key])
+    })
+
+    Object.keys(groupApdSuper).forEach((key) => {
+        supersObject[key] = Object.values(supersObject[key]).concat(groupApdSuper[key])
+    })
+
+
+    const maximo = d3.max(Object.values(supersObject), (d) => d.length);                
+    
+
+    for (var i = 0; i < noteList.length; i++) {
+
+        var nomeSuper = noteList[i].id;
+        var quantidade = Object.values(supersObject[nomeSuper]).length;
+        
+        d3.select(noteList[i])
+        .attr("fill", `hsl(196 70 ${Math.floor((98 * (quantidade)) / maximo)})`)
+        .append("title")
+        .text(nomeSuper + " - " + quantidade);
+    }
+
+}                

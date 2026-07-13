3d52236c7ce06fcd599d65acf11bc653eb1082ea
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-13 19:05:55 -0300

    Update filtrarForm.js

--- modules/filtrarForm.js
+++ modules/filtrarForm.js
@@ -85,7 +85,7 @@ export function filtrarForm (filtros, inputs) {
                 groupSuper = Object.keys(Object.groupBy(values, (item) => item["Super"]));
                 demandaSuper = Object.groupBy(dados, (item) => item["Super"]);
               
-                histogramSvg(dados);   
+                histogramSvg(dados, dataFiles["base.csv"]);   
                 pieSvg(dados, "Tipo");
                 SelectpieSVG(dados);
 
@@ -183,7 +183,7 @@ export function filtrarForm (filtros, inputs) {
 
                 localStorage.setItem(data, total);
 
-                histogramApdSvg(dadosApd);
+                histogramApdSvg(dadosApd, dataFiles["baseapd.csv"]);
                 pieapdSvg(dadosApd, "Tipo IC");
                 SelectpieapdSVG(dadosApd);
                 estadoPlot(demandaSuper, demandaApdSuper);

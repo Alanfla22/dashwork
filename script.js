00fc1bfd1dfab934f54a712dc0bbaf2cd458139b
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-13 15:44:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -20,12 +20,6 @@ input.addEventListener("change", () => {
             filtros.push(filtro);
         };
 
-        d3.select(".superselected")
-        .attr("class", "super");
-
-        d3.selectAll(".super")
-        .style("opacity", "1");
-
         d3.select("#newpath").remove();
 
         d3.select("#mun")
@@ -34,36 +28,7 @@ input.addEventListener("change", () => {
         d3.select("#uf")
         .text("_");
 
-        d3.select("#ufselected")
-        .text(" ");        
-
-        filtrarForm(filtros, inputs);
     });
-
-    d3.selectAll(".super")
-    .on("click", function () {
-
-        const selecao = d3.select(this)._groups[0][0].id;
-
-        const filtro = [["Super", selecao]];
-
-        d3.select(".superselected")
-        .attr("class", "super");
-
-        d3.selectAll(".super")
-        .style("opacity", "0.2");
-
-        d3.select(this)
-        .attr("class", "superselected")
-        .style("opacity", "1");
-
-        d3.select("#ufselected")
-        .text(selecao);            
-
-        filtrarForm(filtro, inputs);
-
-
-    });   
 
 })
 

ca8b28d542a160e574f6f3781c085d1360c57094
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-14 10:48:57 -0300

    Create formulario.js

---
+++ modules/formulario.js
@@ -0,0 +1,91 @@
+
+const field = d3.select("#fieldData");
+const fieldTarefa = d3.select("#fieldTarefa");
+const fieldTipo = d3.select("#fieldTipo");
+const fieldSuper = d3.select("#fieldSuper");
+const noteList = d3.selectAll(".super")._groups[0];
+
+export function formulario (datas, tarefas, tipos) {
+
+
+    field.selectAll("input").remove();
+    field.selectAll("label").remove();
+    field.selectAll("br").remove();
+    
+    fieldTarefa.selectAll("input").remove();
+    fieldTarefa.selectAll("label").remove();
+    fieldTarefa.selectAll("br").remove();
+    
+    fieldTipo.selectAll("input").remove();
+    fieldTipo.selectAll("label").remove();
+    fieldTipo.selectAll("br").remove();
+
+    fieldSuper.selectAll("input").remove();
+    fieldSuper.selectAll("label").remove();
+    fieldSuper.selectAll("br").remove();                      
+
+    const datasArray = Array.from(datas);
+
+    const parseTime = d3.utcParse("%d/%m/%Y");            
+    datasArray.sort((a, b) => parseTime(a) - parseTime(b)); 
+
+    datasArray.forEach((item) => {
+        field.append("input")
+        .attr("type", "checkbox")
+        .attr("name", "Data")
+        .attr("value", item);
+
+        field.append("label")
+        .text(item);
+
+        field.append("br")
+    })
+
+    tarefas.forEach((item) => {
+        fieldTarefa.append("input")
+        .attr("type", "checkbox")
+        .attr("name", "Tarefa")
+        .attr("value", item);
+
+        fieldTarefa.append("label")
+        .text(item);
+
+        fieldTarefa.append("br")
+    })
+    
+    tipos.forEach((item) => {
+        fieldTipo.append("input")
+        .attr("type", "checkbox")
+        .attr("name", "Tipo")
+        .attr("value", item);
+
+        fieldTipo.append("label")
+        .text(item);
+
+        fieldTipo.append("br")
+
+    }) 
+
+    const supers = [];
+
+    noteList.forEach((node) => {
+
+        supers.push(node.id);
+
+    })     
+    
+    supers.forEach((item) => {
+        fieldSuper.append("input")
+        .attr("type", "checkbox")
+        .attr("name", "Super")
+        .attr("value", item);
+
+        fieldSuper.append("label")
+        .text(item);
+
+        fieldSuper.append("br")
+    })                          
+    
+
+
+}

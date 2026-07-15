9abcb5ecea755cb383ee356fe10993e9c5cd69f7
Alanfla22 <114548180+Alanfla22@users.noreply.github.com>
2026-07-15 13:30:32 -0300

    Update filtrar.js

--- modules/filtrar.js
+++ modules/filtrar.js
@@ -1,42 +1,57 @@
-function filtrarItem (item, keysFiltro) {
-    var obj = {};
-    keysFiltro.forEach((key) => {
-        obj[key] = item[key]
-    });
-
-    return new Set(Object.values(obj));
-};
-
 export function filtrar (values, guarda, filtros) {
 
-    const valoresFiltro = new Set();
-    const keysFiltro = new Set();
-
-    filtros.forEach((filtro) => {
-        
-        keysFiltro.add(filtro[0]);
-        valoresFiltro.add(filtro[1]);
-    });
-
-    var result = [];
-
-    values.forEach((item) => {
-
-        var A = filtrarItem(item, keysFiltro);
-        var B = valoresFiltro;
-
-        if (A.isSubsetOf(B)) {
-            result.push(item);
-        };
-
-    });
-
     const listGuarda = Object.keys((Object.groupBy(guarda, (item) => item["CLIENTE"])));
+    
+    if (filtros.length == 0) {
 
-    result.forEach((item) => {
-        item["Guarda"] = listGuarda.includes(item["Nome"]) ? "SIM" : "NAO";
-    })
+        values.forEach((item) => {
+            item["Guarda"] = listGuarda.includes(item["Nome"]) ? "SIM" : "NAO";
+        })
+        
+        return values;
+    } else {
 
+        var objeto = {};
 
-    return result;
+        filtros.forEach((item) => {
+
+            objeto[item[0]] = [];
+
+        })
+
+        filtros.forEach((item) => {
+
+            objeto[item[0]] = Object.values(objeto[item[0]]).concat([item[1]]);
+
+        })
+
+        const selecoes = Object.entries(objeto);
+        const quantidade = selecoes.length;
+
+        const geral = [];
+
+        values.forEach((item) => {
+            var i = 0;
+            selecoes.forEach((x) => {
+                var variavel = item[x[0]];
+                if (x[1].includes(variavel)) {
+                    i = i + 1;                
+                }
+
+                if (i == quantidade) {
+                    geral.push(item);
+                }
+
+            })        
+
+        }) 
+
+        geral.forEach((item) => {
+            item["Guarda"] = listGuarda.includes(item["Nome"]) ? "SIM" : "NAO";
+        })
+
+        return geral; 
+
+   }
+
 };  
